a7381411982b2057c1df2f8c0c0a1b445f79bb21
Giovanni Benner <giovanni.guilioti@benner.com.br>
2022-11-14 15:25:01 -0300

    gameobject class properties

---
+++ SpaceInvaders.GameLogic/GameObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Media;
+
+namespace SpaceInvaders.GameLogic
+{
+    public class GameObject
+    {
+        public Bitmap Sprite { get; set; }
+        public bool Active { get; set; }
+        public int Speed { get; set; }
+        public int Top { get; set; }
+        public int Left { get; set; }
+        public int Width { get { return this.Sprite.Width;} }
+        public int Height { get { return this.Sprite.Height; } }
+        public Size Bounds { get; set; }
+        public Rectangle Rectangle { get; set; }
+        public Stream Sound { get; set; }
+        public Graphics Screen { get; set; }
+        private SoundPlayer soundPlayer { get; set; }
+    }
+}
